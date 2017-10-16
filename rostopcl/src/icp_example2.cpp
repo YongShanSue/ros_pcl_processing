74ab164a32b68ee6f172cb5ab174fbdbc3129c13
yungshansu <michael1994822@gmail.com>
2017-10-16 13:56:55 +0800

    icp_example2

--- rostopcl/src/icp_example2.cpp
+++ rostopcl/src/icp_example2.cpp
@@ -42,22 +42,103 @@ ros::Publisher pointcloud_now_publisher;
 ros::Publisher pointcloud_model_publisher;
 ros::Publisher path_publisher;
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp_model (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp_model(new pcl::PointCloud<pcl::PointXYZ>);
 nav_msgs::Path path;
-
+bool model_is_create;
 
 struct  timeval    start;
 struct  timeval   stop; 
 double time_diff;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
+float x0;
+float yy0;
+float z0;
+
+
+void  hsv_objectdetection(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, const sensor_msgs::ImageConstPtr& image1){
+
+	cv_bridge::CvImagePtr cv_ptr;
+	Mat hsv ,hsv_detected_object ;
+
+    cv_ptr = cv_bridge::toCvCopy(image1, sensor_msgs::image_encodings::BGR8);	
+    Mat hsv_range_mask = Mat::zeros(cv_ptr->image.rows,cv_ptr->image.cols,CV_8U);	
+    Mat hsv_range_mask_2 = Mat::zeros(cv_ptr->image.rows,cv_ptr->image.cols,CV_8U);
+   
+	cvtColor(cv_ptr->image,hsv,CV_BGR2HSV);
+	inRange(hsv,Scalar(170,250,80) , Scalar(180,255,255), hsv_range_mask);		//red range1
+	inRange(hsv,Scalar(0,250,80) , Scalar(1,255,255), hsv_range_mask_2);		//red range2
+	bitwise_or(hsv_range_mask,hsv_range_mask_2, hsv_range_mask_2);
+
+	for(int row=1;row<cv_ptr->image.rows;row++){
+		for(int column=1;column<cv_ptr->image.cols;column++){
+			cv_ptr->image.at<Vec3b>(row,column)[0] *=(hsv_range_mask_2.at<uchar>(row,column)/255.0);
+			cv_ptr->image.at<Vec3b>(row,column)[1] *=(hsv_range_mask_2.at<uchar>(row,column)/255.0);
+			cv_ptr->image.at<Vec3b>(row,column)[2] *=(hsv_range_mask_2.at<uchar>(row,column)/255.0);
+		}
+	}
+	
+
+	//cv_ptr->image.copyTo(test,hsv_range_mask );
+
+	hsv_object_detection.publish(cv_ptr->toImageMsg());
+
+	cv_bridge::CvImagePtr cv_ptr2;
+ 	cv_ptr2 = cv_bridge::toCvCopy(image1, sensor_msgs::image_encodings::BGR8);	
+	cv_ptr2->header   = cv_ptr->header; // Same timestamp and tf frame as input image
+	cv_ptr2->encoding = sensor_msgs::image_encodings::MONO8; // Or whatever
+	cv_ptr2->image    = hsv_range_mask; // Your cv::Mat
+	hsv_mask.publish(cv_ptr2->toImageMsg());
+
+
+	//hsv filtering
+	for(int row=0;row<cv_ptr->image.rows;row++){
+		for(int column=0;column<cv_ptr->image.cols;column++){
+			if(hsv_range_mask.at<uchar>(row+1,column+1)<127){
+				cloud->points[row*cv_ptr->image.cols+column].x = std::numeric_limits<float>::quiet_NaN();
+				cloud->points[row*cv_ptr->image.cols+column].y = std::numeric_limits<float>::quiet_NaN();
+				cloud->points[row*cv_ptr->image.cols+column].z = std::numeric_limits<float>::quiet_NaN();
+			}
+		}
+	}
+	std::vector<int> temp; 
+	printf("Original point Size: %d %d %d\n",cloud->points.size(),cloud->width,cloud->height);	
+	pcl::removeNaNFromPointCloud(*cloud, *cloud, temp); 
+	printf("Filtered point Size: %d %d %d\n",cloud->points.size(),cloud->width,cloud->height);
+}
 
 void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs::ImageConstPtr& image1)
 {	
+
+
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp_final (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_now (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	pcl::fromROSMsg (*input, cloud);
+
+	if(!model_is_create){
+		int model_is_ok = 0;
+		pcl::copyPointCloud(cloud,*cloud_icp_model);
+		hsv_objectdetection(cloud_icp_model,image1);
+		pointcloud_model_publisher.publish(*cloud_icp_model);
+		scanf("%d",&model_is_ok);
+		if(model_is_ok>=1){
+			model_is_create=1;
+			return;
+		}
+		return;
+
+	}
+
+
+	pcl::copyPointCloud(cloud,*cloud_now);
 	gettimeofday(&stop,NULL);
 	time_diff =  (stop.tv_sec-start.tv_sec)+ (stop.tv_usec-start.tv_usec)/1000000.0;
 	printf("wainting_time is %f\n",time_diff);
 	gettimeofday(&start,NULL);
-	
+	/*
+	//sensor_msgs::PointCloud2 pcl_to_ros_pointcloud1;
+	//sensor_msgs::PointCloud2 pcl_to_ros_pointcloud2;
 	 // Create a container for the data.
 	//detect the object
 	cv_bridge::CvImagePtr cv_ptr;
@@ -100,9 +181,10 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs
     
 	gettimeofday(&start,NULL);	
  
-	pcl::PointCloud<pcl::PointXYZ> cloud;
-	pcl::fromROSMsg (*input, cloud);
-	pcl::copyPointCloud(cloud,*cloud_now);
+
+
+
+
 	//hsv filtering
 	for(int row=0;row<cv_ptr->image.rows;row++){
 		for(int column=0;column<cv_ptr->image.cols;column++){
@@ -119,12 +201,24 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs
 	printf("pointcloud_hsv_filtering_time is %f\n",time_diff);
 
 
+	////////Test
+	//pcl::copyPointCloud(*cloud_icp_model,*cloud_now);
+	////for(int i=0;i<cloud_now->width*cloud_now->height;i++){
+	//	cloud_now->points[i].x += 3;
+	//	cloud_now->points[i].y += 4;
+	//	cloud_now->points[i].z += 5;
+	//}
+
+
+
+
 	//gettimeofday(&start,NULL);
 	std::vector<int> temp; 
 	printf("Original point Size: %d %d %d\n",cloud_now->points.size(),cloud_now->width,cloud_now->height);	
 	pcl::removeNaNFromPointCloud(*cloud_now, *cloud_now, temp); 
 	printf("Filtered point Size: %d %d %d\n",cloud_now->points.size(),cloud_now->width,cloud_now->height);
-	
+	*/
+	hsv_objectdetection(cloud_now,image1);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr registration_result (new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
 	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree1 (new pcl::search::KdTree<pcl::PointXYZ>);
@@ -146,7 +240,11 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs
 
 	float x, y, z, roll, pitch, yaw;
 	pcl::getTranslationAndEulerAngles(tROTA, x, y, z, roll, pitch, yaw);	
-	
+	float reg;
+	//reg=x;	x-=(float)x0; x0=reg;
+	//reg=y;	y-=yy0; yy0=reg;
+	//reg=z;	z-=z0; z0=reg;
+
 	geometry_msgs::PoseStamped p;
 	p.header.stamp = ros::Time();
 	printf("%lf %lf %lf \n",x,y,z);
@@ -154,7 +252,8 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs
  	p.pose.position.y = y;
 	p.pose.position.z = z; // 1 meter
 	path.poses.push_back(p);
-	
+	if(path.poses.size()>=10)
+		 path.poses.erase (path.poses.begin());
 
 	path_publisher.publish(path);
 
@@ -181,20 +280,18 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input, const sensor_msgs
 int   main (int argc, char** argv)
 {
      // Initialize ROS
+	model_is_create=0;
 	path.header.frame_id="/map";
-	 pcl::io::loadPCDFile<pcl::PointXYZ> ("1.pcd", *cloud_icp_model);
-	 printf("Model Size: %d %d %d\n",cloud_icp_model->points.size(),cloud_icp_model->width,cloud_icp_model->height);
      ros::init (argc, argv, "my_pcl_tutorial");
      ros::NodeHandle nh;   
-     // Create a ROS subscriber for the input point cloud
-    // ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/camera/depth/points", 1, cloud_cb);
-     // Create a ROS publisher for the output point cloud
      hsv_object_detection = nh.advertise<sensor_msgs::Image> ("hsv_object_detection", 1); 
      hsv_mask = nh.advertise<sensor_msgs::Image> ("hsv_mask", 1); 
      pointcloud_now_publisher= nh.advertise<PointCloudXYZ> ("cloud_now", 1);
      pointcloud_model_publisher = nh.advertise<PointCloudXYZ> ("cloud_icp_model", 1);
      path_publisher = nh.advertise<nav_msgs::Path> ("cloud_path", 1);
-    //pointcloudicp = nh.advertise<PointCloudXYZ> ("icp_result", 1);
+     path.header.frame_id="/camera_link";
+
+     x0=(float)0.0;	yy0=(float)0.0;	z0=(float)0.0;
      ///Synchonizer
      message_filters::Subscriber<sensor_msgs::PointCloud2> depth_point(nh,"/camera/depth/points", 10);
      message_filters::Subscriber<sensor_msgs::Image> image_sub(nh, "/camera/rgb/image_rect_color", 10);
